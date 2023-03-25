945e9389c4b13cb5331cbcd0316a8ae04d97f396
VHNam157 <vhoangnam1507@gmail.com>
2023-03-25 16:26:45 +0700

    first commit

---
+++ ChatGPT_Assistant.py
@@ -0,0 +1,60 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+
+def gpt_speak(content):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(content)
+    engine.runAndWait()
+
+def gpt_listen():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic)
+        audio = recognizer.listen(mic)
+    try:
+        message = recognizer.recognize_google(audio)
+    except sr.UnknownValueError:
+        message = ""
+    return message.lower()
+
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = response.choices[0].text.strip()
+    return message
+
+def chat():
+    print("Welcome to ChatGPT Assistant! Speak or type below:")
+    while True:
+        print("You: ", end="")
+        user_input = gpt_listen()
+        print(user_input)
+        if user_input in ["exit", "quit", "goodbye", "ok bye"]:
+            print("ChatGPT Assistant: Goodbye!")
+            break
+        prompt = f"You: {user_input}\nChatGPT Assistant:"
+        response = generate_text(prompt)
+        print("ChatGPT Assistant:", response)
+        gpt_speak(response)
+
+
+if __name__ == '__main__':
+    API_input = input("Enter your OpenAI API key: ")
+    while True:
+        if API_input.startswith("sk-"):
+            openai.api_key = API_input
+            break
+        else:
+            print("Invalid API key. Please enter a valid key.")
+            API_input = input("Enter your OpenAI API key: ")
+    chat()
+    
---
+++ README.md
@@ -0,0 +1,21 @@
+# **ChatGPT Assistant**
+
+    The simple Python program enables you to talk to GPT. Just enter the your API key and chat with it.
+
+### How to use this dumb_ass Python program
+1. Download and run the file called ChatGPT_Assistant.py
+2. Enter your API key
+3. Talk shjet with GPT    
+
+### How to get your API key?
+To get an API key for OpenAI, you will need to create an account on the OpenAI website (https://openai.com/) and follow these steps:
+
+1. Log in to your OpenAI account.
+2. Go to the API keys page.
+3. Click on the "Create new API key" button.
+4. Enter a name for your API key (optional).
+5. Choose the permissions you want to grant to the API key.
+6. Click on the "Create API key" button.
+7. Copy the generated API key to use in your code.
+
+Note: OpenAI requires you to have a paid subscription to use some of their language models. If you want to use one of these models, you will need to subscribe and provide payment information before you can generate an API key.
