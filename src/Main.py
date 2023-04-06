35ccf30130de1c3c233e50d78d65a73f1c38f401
Vu Hoang Nam <vhoangnam1507@gmail.com>
2023-04-06 20:13:25 +0700

    hok bit nua

--- src/Main.py
+++ src/Main.py
@@ -2,7 +2,7 @@ import GUI
 from ChatGPT_Assistant import ChatGPTAssistant as chatGPT
 
 if __name__=='__main__':
-    API_input = input("Input: ")
+    API_input = input("Input your API key: ")
 
     assistant = chatGPT(API_input)
     assistant.auth()
